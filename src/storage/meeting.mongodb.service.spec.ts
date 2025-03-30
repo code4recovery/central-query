a2e0c4c7677446f9aeecd526f30cfe6eb1576905
Tim Rohrer <tim@tgr66.dev>
2025-03-30 08:19:12 -0700

    feat: Add bySlug query and clean up code.

--- src/storage/meeting.mongodb.service.spec.ts
+++ src/storage/meeting.mongodb.service.spec.ts
@@ -1,57 +1,34 @@
+import fs from "fs"
+import { ObjectId } from "mongodb"
+
 /** TODO: Replace this file with Integration test through Cypress, otherwise update from server-side-demo */
 import { jest } from "@jest/globals"
 
-import { PrevWeekday } from "../utils/dates.js"
 import {
-  addMeetings,
+  bySlug,
   getAllMeetings,
   meetingCollection,
 } from "./meeting.mongodb.service.js"
-import { mongoClient } from "./mongodb-storage-service.js"
+import {
+  configuredMongoDatabase,
+  mongoClient,
+  useCollection,
+} from "./mongodb-storage-service.js"
+import { MeetingModel } from "./storage.types.js"
 
-async function setupDatabase(data: TestData[]) {
-  await meetingCollection.insertMany(data)
-}
+const meetingsTestData = JSON.parse(
+  fs.readFileSync("cypress/fixtures/meetings.json", "utf-8"),
+)
 
-export interface TestData {
-  day: PrevWeekday
-  time: string
-  timezone: string
-  name: string
-}
+// Copilot: Process meetingsTestData and groupsTestData to strip out $oid and associated braces
+const cleanedMeetingsTestData = meetingsTestData.map((meeting) => {
+  meeting.groupID = new ObjectId(meeting.groupID.$oid)
+  return meeting
+})
 
-const testEvents: TestData[] = [
-  {
-    name: "Su-1830", // 2330Z; in DST 22:30Z
-    time: "18:30",
-    day: 0,
-    timezone: "America/New_York",
-  },
-  {
-    name: "Su-2300", // 2300Z
-    time: "23:00",
-    day: 0,
-    timezone: "Atlantic/Reykjavik",
-  },
-  {
-    name: "Su-1645", // 2345Z
-    time: "16:45",
-    day: 0,
-    timezone: "America/Phoenix",
-  },
-  {
-    name: "Su-2200", // 0300Z on Monday; in DST 0200Z on Monday
-    time: "22:00",
-    day: 0,
-    timezone: "America/New_York",
-  },
-  {
-    name: "Su-1500", // 2000Z; in DST 1900Z
-    time: "15:00",
-    day: 0,
-    timezone: "America/New_York",
-  },
-]
+const testMeetingsCollection = useCollection("meeting")<MeetingModel>(
+  configuredMongoDatabase,
+)
 
 async function resetDatabase() {
   await meetingCollection.deleteMany({})
@@ -65,11 +42,16 @@ afterAll(async () => {
   jest.useRealTimers()
 })
 
-test("getAllMeetings returns five documents", async () => {
-  await setupDatabase(testEvents)
-  expect(await getAllMeetings()).toHaveLength(5)
+test("getAllMeetings returns all documents", async () => {
+  await testMeetingsCollection.insertMany(cleanedMeetingsTestData)
+  expect(await getAllMeetings()).toHaveLength(cleanedMeetingsTestData.length)
 })
 
-test("addMeetings works", async () => {
-  expect((await addMeetings(testEvents)).insertedCount).toBe(5)
+test("bySlug returns a single document", async () => {
+  await testMeetingsCollection.insertMany(cleanedMeetingsTestData)
+
+  const result = await bySlug("global-mens-meditation-6")
+
+  expect(result).not.toBeNull()
+  expect(result!.name).toBe("Global Men's Meditation")
 })
--- src/storage/meeting.mongodb.service.ts
+++ src/storage/meeting.mongodb.service.ts
@@ -5,7 +5,7 @@ import {
   configuredMongoDatabase,
   useCollection,
 } from "./mongodb-storage-service.js"
-import { Meeting, MeetingModel } from "./storage.types.js"
+import { MeetingModel } from "./storage.types.js"
 
 export const meetingCollection = useCollection("meeting")<MeetingModel>(
   configuredMongoDatabase,
@@ -15,20 +15,11 @@ const meetingView = useCollection("meeting-view")<MeetingModel>(
   configuredMongoDatabase,
 )
 
-export const addMeetings = async (resources: Meeting[]) =>
-  await meetingCollection.insertMany(resources)
-
 export const getAllMeetings = async () =>
   await meetingCollection.find({}).toArray()
 
 export const dayOfWeek = async (day: number) =>
   await meetingCollection.find({ day: day }).toArray()
-
-// const loadPipeline = (pipeline: MongoDB.Document[]) =>
-//   performAggregationUsingPipeline(pipeline).toArray()
-
-// const performAggregationUsingPipeline = (pipeline: MongoDB.Document[]) =>
-//   meetingCollection.aggregate(pipeline)
 
 const pipelineView = (pipeline: MongoDB.Document[]) =>
   meetingView.aggregate(pipeline)
@@ -38,6 +29,9 @@ const loadPipelineView = (pipeline: MongoDB.Document[]) =>
 
 export const query = async (queryPipeline: MongoDB.Document[]) =>
   loadPipelineView(queryPipeline)
+
+export const bySlug = async (slug: string) =>
+  meetingCollection.findOne({ slug })
 
 export const byDay = async (day: Weekdays) => {
   const searchDay = day.toString()
