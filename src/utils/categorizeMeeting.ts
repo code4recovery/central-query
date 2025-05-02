5a9063df47c069d6d0ac86b69cedfd320b9c6fda
Tim Rohrer <tim@tgr66.dev>
2025-05-02 09:34:21 -0700

    refactor: Move local types to common.

--- src/utils/categorizeMeeting.ts
+++ src/utils/categorizeMeeting.ts
@@ -1,75 +1,19 @@
-import { MeetingModel } from "../storage/storage.types"
-
-export const TYPE = ["O", "C"] // 2
-export const FORMATS = [
-  "11",
-  "12x12",
-  "A",
-  "ABSI",
-  "B",
-  "BE",
-  "D",
-  "DR",
-  "GR",
-  "H",
-  "LIT",
-  "LS",
-  "MED",
-  "SP",
-  "ST",
-  "TR",
-] // 16
-export const FEATURES = [
-  "AL-AN",
-  "AL",
-  "ASL",
-  "BA",
-  "BRK",
-  "CAN",
-  "CF",
-  "DB",
-  "X",
-  "XB",
-  "XT",
-  "POA",
-  "OUT",
-  "FF",
-  "RSL",
-  "TC",
-] // 15
-export const COMMUNITIES = [
-  "M",
-  "W",
-  "DD",
-  "LGBTQ",
-  "Y",
-  "N",
-  "BI",
-  "T",
-  "SEN",
-  "POC",
-  "P",
-  "NDG",
-  "L",
-  "G",
-  "BV-I",
-  "D-HOH",
-  "LO-I",
-] // 18
-
-export type Community = (typeof COMMUNITIES)[keyof typeof COMMUNITIES]
-export type Feature = (typeof FEATURES)[keyof typeof FEATURES]
-export type Format = (typeof FORMATS)[keyof typeof FORMATS]
-export type Type = (typeof TYPE)[keyof typeof TYPE]
-
-export type Category = Community | Feature | Format | Type
+import {
+  Category,
+  COMMUNITIES,
+  FEATURES,
+  FORMATS,
+  Type,
+  TYPE,
+} from "../common/types"
+import { MeetingView } from "../storage/storage.types"
 
 function intersection<T>(arr1: T[], arr2: T[]): T[] {
   const set1 = new Set(arr1)
   return arr2.filter((value) => set1.has(value))
 }
 
-export function categorizedMeeting(meeting: MeetingModel) {
+export function categorizedMeeting(meeting: MeetingView) {
   const { types } = meeting
 
   delete meeting.types
@@ -79,6 +23,6 @@ export function categorizedMeeting(meeting: MeetingModel) {
     communities: intersection<Category>(types, COMMUNITIES),
     features: intersection<Category>(types, FEATURES),
     formats: intersection<Category>(types, FORMATS),
-    type: intersection<Category>(types, TYPE),
+    type: intersection<Category>(types, TYPE)[0] as Type,
   }
 }
