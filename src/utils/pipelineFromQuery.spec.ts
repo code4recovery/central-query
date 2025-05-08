81cbb8ee2bf754f6961b37be6bcdf44d194eb817
Tim Rohrer <tim@tgr66.dev>
2025-05-08 07:21:41 -0700

    refactor: Change how pipeline query is built.
    This reflects major changes to the pipeline.

--- src/utils/pipelineFromQuery.spec.ts
+++ src/utils/pipelineFromQuery.spec.ts
@@ -1,8 +1,8 @@
-import { PipelineFields } from "../endpoint-options.types.js"
+import { MeetingsOptions } from "../endpoint-options.types.js"
 import { pipelineFromQuery } from "./pipelineFromQuery.js"
 
 test("pipeline should reflect same day time range", () => {
-  const testRanges: PipelineFields = {
+  const testRanges: MeetingsOptions = {
     rtcRanges: [
       {
         lowerRTC: "7:01:00",
@@ -13,13 +13,13 @@ test("pipeline should reflect same day time range", () => {
   expect(pipelineFromQuery(testRanges)).toStrictEqual([
     {
       $match: {
-        $and: [{ rtc: { $gte: "7:01:00" } }, { rtc: { $lte: "7:03:00" } }],
+        rtc: { $gte: "7:01:00", $lte: "7:03:00" },
       },
     },
   ])
 })
 test("pipeline should reflect Sunday => Monday", () => {
-  const testRanges: PipelineFields = {
+  const testRanges: MeetingsOptions = {
     rtcRanges: [
       {
         lowerRTC: "7:23:00",
@@ -45,6 +45,98 @@ test("pipeline should reflect Sunday => Monday", () => {
             rtc: {
               $gte: "1:00:00",
               $lte: "1:01:00",
+            },
+          },
+        ],
+      },
+    },
+  ])
+})
+test("pipeline should reflect correct types for the query", () => {
+  const testOptions: MeetingsOptions = {
+    formats: ["D", "B", "BE"],
+    communities: ["M"],
+    type: "O",
+  }
+
+  expect(pipelineFromQuery(testOptions)).toStrictEqual([
+    {
+      $match: {
+        types: {
+          $all: ["D", "B", "BE", "M", "O"],
+        },
+      },
+    },
+  ])
+})
+test("pipeline should reflect correct types for the query when rtcRanges are present", () => {
+  const testRanges: MeetingsOptions = {
+    rtcRanges: [
+      {
+        lowerRTC: "7:23:00",
+        upperRTC: "7:24:00",
+      },
+    ],
+    formats: ["D", "B", "BE"],
+    communities: ["M"],
+    type: "O",
+  }
+
+  expect(pipelineFromQuery(testRanges)).toStrictEqual([
+    {
+      $match: {
+        $and: [
+          { rtc: { $gte: "7:23:00", $lte: "7:24:00" } },
+          {
+            types: {
+              $all: ["D", "B", "BE", "M", "O"],
+            },
+          },
+        ],
+      },
+    },
+  ])
+})
+test("pipeline should be correct when types are present and RTC transitions from Sunday ==> Monday", () => {
+  const testData: MeetingsOptions = {
+    rtcRanges: [
+      {
+        lowerRTC: "7:23:00",
+        upperRTC: "7:24:00",
+      },
+      {
+        lowerRTC: "1:00:00",
+        upperRTC: "1:01:00",
+      },
+    ],
+    formats: ["D", "B", "BE"],
+    communities: ["M"],
+    type: "O",
+  }
+
+  expect(pipelineFromQuery(testData)).toStrictEqual([
+    {
+      $match: {
+        $and: [
+          {
+            $or: [
+              {
+                rtc: {
+                  $gte: "7:23:00",
+                  $lte: "7:24:00",
+                },
+              },
+              {
+                rtc: {
+                  $gte: "1:00:00",
+                  $lte: "1:01:00",
+                },
+              },
+            ],
+          },
+          {
+            types: {
+              $all: ["D", "B", "BE", "M", "O"],
             },
           },
         ],
--- src/utils/pipelineFromQuery.ts
+++ src/utils/pipelineFromQuery.ts
@@ -1,86 +1,71 @@
 import * as MongoDB from "mongodb"
 
 import Logger from "../common/logger.js"
-import { PipelineFields } from "../endpoint-options.types.js"
+import { MeetingsOptions } from "../endpoint-options.types.js"
 
-export const pipelineFromQuery = (query: PipelineFields) => {
+export const pipelineFromQuery = (query: MeetingsOptions | MeetingsOptions) => {
   const pipeline: MongoDB.Document[] = []
   const { rtcRanges, limit, formats, features, communities, type } = query
 
+  Logger.debug(`Formats: ${formats}`)
+  Logger.debug(`Features: ${features}`)
+  Logger.debug(`Communities: ${communities}`)
+  Logger.debug(`Type: ${type}`)
+  Logger.debug(`RTC Ranges: ${rtcRanges}`)
+  Logger.debug(`Limit: ${limit}`)
+  Logger.debug(`Query: ${JSON.stringify(query)}`)
+
   let match: Record<string, unknown> = {}
 
-  // Merge formats, features, communities, and type into a single `types` array
+  const normalizeToArray = (input: string | string[] | undefined): string[] =>
+    input ? (Array.isArray(input) ? input : [input]) : []
+
+  // Merge formats, features, communities, and type into a single `types` array to comply with the database schema
   const mergedTypes = [
-    ...(formats || []),
-    ...(features || []),
-    ...(communities || []),
-    ...(type ? [type] : []),
+    ...normalizeToArray(formats),
+    ...normalizeToArray(features),
+    ...normalizeToArray(communities),
+    ...normalizeToArray(type),
   ]
+  Logger.debug(`Merged types: ${mergedTypes}`)
 
-  if (rtcRanges.length === 1) {
-    match = {
-      $and: [
-        { rtc: { $gte: rtcRanges[0].lowerRTC } },
-        { rtc: { $lte: rtcRanges[0].upperRTC } },
-      ],
-    }
-  } else {
-    match = {
-      $or: [
-        {
-          rtc: {
-            $gte: rtcRanges[0].lowerRTC,
-            $lte: rtcRanges[0].upperRTC,
-          },
+  if (rtcRanges && rtcRanges.length > 0) {
+    if (rtcRanges.length === 1) {
+      match = {
+        rtc: {
+          $gte: rtcRanges[0].lowerRTC,
+          $lte: rtcRanges[0].upperRTC,
         },
-        {
+      }
+    } else {
+      match = {
+        $or: rtcRanges.map((range) => ({
           rtc: {
-            $gte: rtcRanges[1].lowerRTC,
-            $lte: rtcRanges[1].upperRTC,
+            $gte: range.lowerRTC,
+            $lte: range.upperRTC,
           },
-        },
-      ],
+        })),
+      }
     }
   }
 
-  if (mergedTypes.length > 0) {
-    match = {
-      ...match,
-      types: { $all: mergedTypes },
-    }
-  }
+  const updatedMatch =
+    mergedTypes.length > 0 && Object.keys(match).length > 0
+      ? {
+          $and: [
+            match, // Combine `rtc` conditions into a single object
+            { types: { $all: mergedTypes } },
+          ],
+        }
+      : mergedTypes.length > 0
+      ? { types: { $all: mergedTypes } }
+      : Object.keys(match).length > 0
+      ? match
+      : {}
 
-  pipeline.push({ $match: match })
+  pipeline.push({ $match: updatedMatch })
+
   if (limit != undefined) pipeline.push({ $limit: limit })
   Logger.debug(`pipeline built: ${JSON.stringify(pipeline)}`)
   return pipeline
 }
-
-// const preparedPipeline = () => {
-//   let match = {}
-//   if (q.timezone) {
-//     match = {
-//       ...match,
-//       timezone: q.timezone,
-//     }
-//   }
-//   if (q.weekday) {
-//     const searchDay = Weekdays[q.weekday as string]
-//     match = {
-//       ...match,
-//       rtc: { $regex: `^${searchDay}` },
-//     }
-//   }
-//   if (q.timeEqual && q.timezone && q.weekday) {
-//     const day = Weekdays[q.weekday as string]
-//     const now = DateTime.now()
-//     const next = nextOccurrence(day, now)
-//     // console.log(next)
-//     console.log(DateTime.fromISO(next.toISO(), { zone: q.timezone as string }))
-//   }
-//   return [
-//     {
-//       $match: match,
-//     },
-//   ]
-// }
